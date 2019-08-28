c294b68417a0663f00db952ca2dc968efd6acc9d
Lucas F. Souza <me@lucasfsouza.com.br>
2019-08-28 01:06:09 -0300

    fix launcher

--- .config/kitty/launcher.sh
+++ .config/kitty/launcher.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-selection="$(dot-desktop | fzf --print-query | sed '/^[[:space:]]*$/d')"
+readarray -t lines < <(echo "$(dot-desktop | fzf --print-query)")
+query="${lines[0]}"
+selection="${lines[1]}"
 
 if [ "$selection" != "" ]
 then
     command_str="$(dot-desktop "${selection}")"
-
-    if [ "$command_str" == "" ]
-    then
-        # No desktop file matched, raw command line
-        command_str="${selection}"
-    fi
-
-    swaymsg -t command "exec $command_str"
-
-    exit 0
+elif [ "$query" != "" ]
+then
+    command_str="${query}"
+else
+  exit 1
 fi
 
-exit 1
+swaymsg -t command "exec $command_str"
+exit 0
